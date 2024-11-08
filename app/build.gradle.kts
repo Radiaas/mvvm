201d268539ce0adaf7eef8998347238eef5f030a
Radiaas <radiasadna@gmail.com>
2024-11-09 06:57:04 +0700

    aplikasi praktikum2

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -26,6 +26,7 @@ android {
             )
         }
     }
+
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_1_8
         targetCompatibility = JavaVersion.VERSION_1_8
@@ -33,13 +34,14 @@ android {
     kotlinOptions {
         jvmTarget = "1.8"
     }
+
     buildFeatures {
         viewBinding = true
+        dataBinding = true
     }
 }
 
 dependencies {
-
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.appcompat)
     implementation(libs.material)
@@ -48,4 +50,5 @@ dependencies {
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
+    implementation (libs.androidx.xfragment.ktx)
 }
--- app/src/main/java/com/example/mvvm/MainActivity.kt
+++ app/src/main/java/com/example/mvvm/MainActivity.kt
@@ -1,36 +1,21 @@
 package com.example.mvvm
 
 import android.os.Bundle
-import android.view.View
+import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
+import androidx.databinding.DataBindingUtil
 import com.example.mvvm.databinding.ActivityMainBinding
 
-class MainActivity : AppCompatActivity(), View.OnClickListener {
-
-    private var counter = 0
+class MainActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityMainBinding
 
+    private val viewModel: MainViewModel by viewModels()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        binding = ActivityMainBinding.inflate(layoutInflater)
-        setContentView(binding.root)
-    }
-
-    private fun increaseCounter() {
-        counter += 1
-        binding.tvCounter.text = "$counter"
-    }
-
-    private fun decreaseCounter() {
-        counter -= 1
-        binding.tvCounter.text = "$counter"
-    }
-
-    override fun onClick(p0: View?) {
-        when (p0) {
-            binding.btnPlus -> increaseCounter()
-            binding.btnMinus -> decreaseCounter()
-        }
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
+        binding.viewModel = viewModel
     }
 }
+
