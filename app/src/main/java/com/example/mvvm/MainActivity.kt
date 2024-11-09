39b03e660cca251170b928696e5dc3bda95bc85e
Radiaas <radiasadna@gmail.com>
2024-11-09 07:53:21 +0700

    aplikasi praktikum3

--- app/src/main/java/com/example/mvvm/MainActivity.kt
+++ app/src/main/java/com/example/mvvm/MainActivity.kt
@@ -4,18 +4,22 @@ import android.os.Bundle
 import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
 import androidx.databinding.DataBindingUtil
+import com.example.mvvm.Data.SaveCounter
 import com.example.mvvm.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityMainBinding
 
-    private val viewModel: MainViewModel by viewModels()
+    private val viewModel: MainViewModel by viewModels {
+        MainViewModel.Factory(SaveCounter(this))
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
         binding.viewModel = viewModel
+
+        viewModel.sendValue()
     }
 }
-
--- app/src/main/java/com/example/mvvm/MainViewModel.kt
+++ app/src/main/java/com/example/mvvm/MainViewModel.kt
@@ -2,22 +2,32 @@ package com.example.mvvm
 
 import androidx.databinding.ObservableField
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.ViewModelProvider
+import com.example.mvvm.Data.SaveCounter
 
-class MainViewModel : ViewModel() {
+class MainViewModel(private val saveCounter: SaveCounter) : ViewModel() {
 
     val counter = ObservableField(0)
 
     fun increaseCounter() {
-        val current = counter.get() ?: 0
-        counter.set(current + 1)
+        saveCounter.counter = saveCounter.counter + 1
+        sendValue()
     }
 
     fun decreaseCounter() {
-        val current = counter.get() ?: 0
-        counter.set(current - 1)
+        saveCounter.counter = saveCounter.counter - 1
+        sendValue()
+    }
+
+    fun sendValue() {
+        counter.set(saveCounter.counter)
+    }
+
+    class Factory(private val saveCounter: SaveCounter) : ViewModelProvider.Factory {
+        override fun <T : ViewModel> create(modelClass: Class<T>): T {
+            return MainViewModel(saveCounter) as T
+        }
     }
 }
-
-
 
 
